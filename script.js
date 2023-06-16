eb03379fb235675d694077f864e60cfc79cc5a37
azizbek2OO7 <malikovazizbek726@gmail.com>
2023-06-16 23:20:31 +0500

    calculator

---
+++ script.js
@@ -0,0 +1,23 @@
+const input = document.getElementById("inputBox");
+const buttons = document.querySelectorAll("button");
+
+let string = "";
+let arr = Array.from(buttons);
+
+arr.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML === "=") {
+      string = eval(string);
+      input.value = string;
+    } else if (e.target.innerHTML === "AC") {
+      string = "";
+      input.value = string;
+    } else if (e.target.innerHTML === "DEL") {
+      string = string.slice(0, -1);
+      input.value = string;
+    } else {
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
